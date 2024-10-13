b3c8ef0914302e1cd77edd79a8cf614e60192a79
Ruslan <ruslan.kuga@keyconcept.at>
2024-10-13 13:32:34 +0300

    change main

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	auth "auth/pkg/user_v1"
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	auth.UnimplementedUserV1Server
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	auth.RegisterUserV1Server(s, &server{})
+
+	log.Printf("Listening")
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("falied to serve")
+	}
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,12 @@ module auth
 go 1.23
 
 require (
-	github.com/fatih/color v1.17.0
 	github.com/golang/protobuf v1.5.4
 	google.golang.org/grpc v1.67.1
 	google.golang.org/protobuf v1.35.1
 )
 
 require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
